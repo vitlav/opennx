ccade5f97e53f3765055a4004da4ad797c5e39fc
felfert <felfert@585af7c1-6c24-0410-985e-cb98ac3fd56a>
2009-03-24 14:51:21 +0000

    - Fixed #ifdef
    git-svn-id: https://opennx.svn.sourceforge.net/svnroot/opennx/trunk@204 585af7c1-6c24-0410-985e-cb98ac3fd56a

--- opennx/opennxApp.cpp
+++ opennx/opennxApp.cpp
@@ -545,7 +545,7 @@ opennxApp::preInit()
     wxString ldpath;
     if (::wxGetEnv(wxT("LD_LIBRARY_PATH"), &ldpath))
         ldpath += wxT(":");
-#ifdef defined(__x86_64) || defined(__IA64__)
+#if defined(__x86_64) || defined(__IA64__)
     ldpath = tmp + wxT("/lib64");
 #else
     ldpath = tmp + wxT("/lib");
