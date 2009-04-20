294753af6c940123a2b6e282d6ebdaec38316bf9
felfert <felfert@585af7c1-6c24-0410-985e-cb98ac3fd56a>
2009-04-20 14:13:14 +0000

    - Make shure, that trace flags are initialized in    proper sequence. Fixes call of watchusbip returning    with usage output.
    git-svn-id: https://opennx.svn.sourceforge.net/svnroot/opennx/trunk@239 585af7c1-6c24-0410-985e-cb98ac3fd56a

--- opennx/trace.h
+++ opennx/trace.h
@@ -9,20 +9,21 @@
 #include <string>
 #include <wx/filename.h>
 
-extern std::vector<std::string> _allTraceTags;
+extern std::vector<std::string> *_allTraceTags;
 
 #define ENABLE_TRACE \
     static wxString MYTRACETAG(wxFileName::FileName(wxT(__FILE__)).GetName()); \
-    static void __attribute__((constructor)) _trinit() { \
-        _allTraceTags.push_back(std::string(__FILE__)); \
+    static void __attribute__ ((constructor)) _trinit() { \
+        if (!_allTraceTags) _allTraceTags = new std::vector<std::string>(); \
+        _allTraceTags->push_back(std::string(__FILE__)); \
     }
 
 #define DECLARE_TRACETAGS \
-    std::vector<std::string> _allTraceTags; \
+    std::vector<std::string> *_allTraceTags = NULL; \
     static wxArrayString allTraceTags; \
     static void initWxTraceTags() { \
-        std::vector<std::string>::iterator i; \
-       for (i = _allTraceTags.begin(); i != _allTraceTags.end(); i++) { \
+       std::vector<std::string>::iterator i; \
+       for (i = _allTraceTags->begin(); i != _allTraceTags->end(); i++) { \
            wxString tag(i->c_str(), wxConvUTF8); \
            allTraceTags.Add(wxFileName::FileName(tag).GetName()); \
        } \
