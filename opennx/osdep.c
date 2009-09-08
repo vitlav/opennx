3075b3d6f65bfe2654b323d2e9ee8933452d185b
felfert <felfert@585af7c1-6c24-0410-985e-cb98ac3fd56a>
2009-09-08 13:27:18 +0000

    - MacOS: Fixed bug in startup of quartz-wm
    git-svn-id: https://opennx.svn.sourceforge.net/svnroot/opennx/trunk@393 585af7c1-6c24-0410-985e-cb98ac3fd56a

--- opennx/osdep.c
+++ opennx/osdep.c
@@ -250,11 +250,11 @@ static Display *launchX11() {
     putenv("DISPLAY=:0");
     /* If the following fails, there is usually a WM already running
      * so we don't care ... */
-    if (access("/usr/X11R6/bin/quartz-wm", X_OK)) {
+    if (0 == access("/usr/X11R6/bin/quartz-wm", X_OK)) {
         system("/usr/X11R6/bin/quartz-wm &");
         return ret;
     }
-    if (access("/usr/bin/quartz-wm", X_OK)) {
+    if (0 == access("/usr/bin/quartz-wm", X_OK)) {
         system("/usr/bin/quartz-wm &");
         return ret;
     }
